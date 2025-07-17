61b011d0154cb884b7b3701784699a1b42186245
chihaamin <amin.chiha@gmail.com>
2025-07-17 21:32:15 +0100

    Implement _putchar and _printf functions in separate source files

---
+++ _printf.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _printf - A function that mimics the behavior of printf.
+ * @format: The format string containing characters and format specifiers.
+ *
+ * Description: This function produces output according to a format string,
+ *              similar to the standard printf function. It supports various
+ *              format specifiers for formatting data.
+ *
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings).
+ */
+int _printf(const char *format, ...)
+{
+	(void)format;
+	return (0);
+}
--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 int _putchar(char c);
 
